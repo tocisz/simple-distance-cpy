802e3db6d02d6f6955f42d284bf579c1ac69b0d2
tci <cichymail@gmail.com>
2016-04-07 22:49:09 +0200

    h outside

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from distutils.core import setup, Extension
 
 distance_wrap = Extension('_distance_wrap',
-                           sources = ['distance_wrap.c'])
+    sources = ['distance_wrap.c'],
+    include_dirs=['.heroku/python/lib/python3.5/site-packages/numpy/core/include/numpy/_numpyconfig.h']
+    )
 
 setup(name = 'simple-distance-cpy',
-      version = '0.11',
+      version = '0.12',
       description = 'I want euclidean cdist and I want it fast',
       ext_modules = [distance_wrap],
 
