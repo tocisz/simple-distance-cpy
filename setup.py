3b4b7adc1457e50c0c60c590f797a9317e901f88
tci <cichymail@gmail.com>
2016-04-07 23:37:31 +0200

    python code

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup, Extension
 
-distance_wrap = Extension('_distance_wrap',
+distance_wrap = Extension('distance._distance_wrap',
     sources = ['distance_wrap.c'],
     include_dirs=['/app/.heroku/python/lib/python3.5/site-packages/numpy/core/include'],
     library_dirs=['/app/.heroku/python/lib/python3.5/site-packages/numpy/core/lib'],
@@ -8,8 +8,9 @@ distance_wrap = Extension('_distance_wrap',
     )
 
 setup(name = 'simple-distance-cpy',
-      version = '0.15',
-      description = 'I want euclidean cdist and I want it fast',
+      version = '0.16',
+      description = 'I want euclidean cdist and I want it now',
+      py_modules = ['distance'],
       ext_modules = [distance_wrap],
 
       url='https://github.com/tocisz/simple-distance-cpy',
