37d6b7fa843c48902441be141909eda836a6ee2f
tci <cichymail@gmail.com>
2016-04-07 22:57:45 +0200

    fix path

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup, Extension
 
 distance_wrap = Extension('_distance_wrap',
     sources = ['distance_wrap.c'],
-    include_dirs=['.heroku/python/lib/python3.5/site-packages/numpy/core/include/numpy/_numpyconfig.h']
+    include_dirs=['/app/.heroku/python/lib/python3.5/site-packages/numpy/core/include/numpy/_numpyconfig.h']
     )
 
 setup(name = 'simple-distance-cpy',
-      version = '0.13',
+      version = '0.14',
       description = 'I want euclidean cdist and I want it fast',
       ext_modules = [distance_wrap],
 
