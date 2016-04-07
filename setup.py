33bcd69666b2cafeaf82b908f3aae371017e209d
tci <cichymail@gmail.com>
2016-04-07 22:59:42 +0200

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 
 distance_wrap = Extension('_distance_wrap',
     sources = ['distance_wrap.c'],
-    include_dirs=['/app/.heroku/python/lib/python3.5/site-packages/numpy/core/include/numpy/_numpyconfig.h']
+    include_dirs=['/app/.heroku/python/lib/python3.5/site-packages/numpy/core/include']
     )
 
 setup(name = 'simple-distance-cpy',
