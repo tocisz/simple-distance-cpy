2bcea35db291871eae9248f8d4485dfbfd0d3bba
tci <cichymail@gmail.com>
2016-04-07 23:50:59 +0200

    forgot to increase nr

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ distance_wrap = Extension('_distance_wrap',
     )
 
 setup(name = 'simple-distance-cpy',
-      version = '0.17',
+      version = '0.18',
       description = 'I want euclidean cdist and I want it now',
       py_modules = ['distance'],
       ext_modules = [distance_wrap],
